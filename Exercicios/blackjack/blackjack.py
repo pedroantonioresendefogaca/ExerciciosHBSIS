a679901aaf2493f865eddb213b63f2d84576407d
Pedro Antonio Resende Fogaça <pedro.fogaca@hbsis.com.br>
2020-03-13 20:50:06 -0300

    Atualizando as regras

--- Exercicios/blackjack/blackjack.py
+++ Exercicios/blackjack/blackjack.py
@@ -12,22 +12,32 @@ class Baralho:
             try:
                 menu_selecao = int(input('1 - Retirar carta\n2 - Sair\nSelecione a opção desejada: '))
                 if menu_selecao == 1:
-                    lista_cartas_chaves = list(dict.keys(dict_cartas))
-                    lista_cartas_valores = list(dict.values(dict_cartas))
                     numeros = random.choice(list(dict.keys(dict_cartas)))
                     if numeros is not int:
+                        if numeros == 'A':
+                            if not lista_pontuacao:
+                                del(dict_cartas['A'])
+                                dict_cartas.update({'A':11})
+                                lista_pontuacao.append(dict_cartas[numeros])
+                            else:
+                                pass
+                        else:
+                            pass
                         lista_pontuacao.append(dict_cartas[numeros])
                     else:
                         lista_pontuacao.append(numeros)
                     print(f'A carta retirada foi {numeros}')
                     print(f'Sua pontuação atual é de {sum(lista_pontuacao)} pontos!')
                 elif menu_selecao == 2:
-                    print('GAME OVER')
+                    print(f'Sua pontuação foi de {sum(lista_pontuacao)} pontos, obrigado por jogar!')
                     break
                 else:
                     print('Insira uma operação válida')
-                if sum(lista_pontuacao) >= 21:
+                if sum(lista_pontuacao) == 21:
                     print('Parabéns! Você ganhou!')
+                    break
+                elif sum(lista_pontuacao) > 21:
+                    print('Que pena! Você perdeu!')
                     break
                 else:
                     pass
