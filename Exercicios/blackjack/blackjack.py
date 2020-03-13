7f001fe498f4a2f7d424a35ade9042e829d19863
Pedro Antonio Resende Fogaça <pedro.fogaca@hbsis.com.br>
2020-03-13 19:48:12 -0300

    Correção de ValueError

--- Exercicios/blackjack/blackjack.py
+++ Exercicios/blackjack/blackjack.py
@@ -9,28 +9,30 @@ class Baralho:
         dict_cartas = self.__dict_cartas
         lista_pontuacao = self.__lista_pontuacao
         while True:
-            menu_selecao = int(input('1 - Retirar carta\n2 - Sair\nSelecione a opção desejada: '))
-            if menu_selecao == 1:
-                lista_cartas_chaves = list(dict.keys(dict_cartas))
-                lista_cartas_valores = list(dict.values(dict_cartas))
-                numeros = random.choice(list(dict.keys(dict_cartas)))
-                if numeros is not int:
-                    lista_pontuacao.append(dict_cartas[numeros])
+            try:
+                menu_selecao = int(input('1 - Retirar carta\n2 - Sair\nSelecione a opção desejada: '))
+                if menu_selecao == 1:
+                    lista_cartas_chaves = list(dict.keys(dict_cartas))
+                    lista_cartas_valores = list(dict.values(dict_cartas))
+                    numeros = random.choice(list(dict.keys(dict_cartas)))
+                    if numeros is not int:
+                        lista_pontuacao.append(dict_cartas[numeros])
+                    else:
+                        lista_pontuacao.append(numeros)
+                    print(f'A carta retirada foi {numeros}')
+                    print(f'Sua pontuação atual é de {sum(lista_pontuacao)} pontos!')
+                elif menu_selecao == 2:
+                    print('GAME OVER')
+                    break
                 else:
-                    lista_pontuacao.append(numeros)
-                print(f'A carta retirada foi {numeros}')
-                print(f'Sua pontuação atual é de {sum(lista_pontuacao)} pontos!')
-            elif menu_selecao == 2:
-                print('GAME OVER')
-                break
-            else:
+                    print('Insira uma operação válida')
+                if sum(lista_pontuacao) >= 21:
+                    print('Parabéns! Você ganhou!')
+                    break
+                else:
+                    pass
+            except ValueError:
                 print('Insira uma operação válida')
-            if sum(lista_pontuacao) >= 21:
-                print('Parabéns! Você ganhou!')
-                break
-            else:
-                pass
-
+                
 testando = Baralho()
 testando.jack()
-
