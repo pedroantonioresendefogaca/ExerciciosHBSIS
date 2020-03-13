6b1ad2fbd0501ad521441cdc8ae52c10369340c6
Pedro Antonio Resende Fogaça <pedro.fogaca@hbsis.com.br>
2020-03-13 19:32:58 -0300

    Inicial commit

---
+++ Exercicios/blackjack/blackjack.py
@@ -0,0 +1,36 @@
+import random
+
+class Baralho:
+    def __init__(self):
+        self.__dict_cartas = {'A':1, 2:2,3:3,4:4,5:5,6:6,7:7,8:8,9:9,10:10,'J':10,'Q':10,'K':10}
+        self.__lista_pontuacao = []
+
+    def jack(self):
+        dict_cartas = self.__dict_cartas
+        lista_pontuacao = self.__lista_pontuacao
+        while True:
+            menu_selecao = int(input('1 - Retirar carta\n2 - Sair\nSelecione a opção desejada: '))
+            if menu_selecao == 1:
+                lista_cartas_chaves = list(dict.keys(dict_cartas))
+                lista_cartas_valores = list(dict.values(dict_cartas))
+                numeros = random.choice(list(dict.keys(dict_cartas)))
+                if numeros is not int:
+                    lista_pontuacao.append(dict_cartas[numeros])
+                else:
+                    lista_pontuacao.append(numeros)
+                print(f'A carta retirada foi {numeros}')
+                print(f'Sua pontuação atual é de {sum(lista_pontuacao)} pontos!')
+            elif menu_selecao == 2:
+                print('GAME OVER')
+                break
+            else:
+                print('Insira uma operação válida')
+            if sum(lista_pontuacao) >= 21:
+                print('Parabéns! Você ganhou!')
+                break
+            else:
+                pass
+
+testando = Baralho()
+testando.jack()
+
---
+++ Exercicios/blackjack/problemablackjack.py
@@ -0,0 +1,25 @@
+'''As cartas utilizadas e valores serão conforme abaixo:
+"2" => 2
+"3" => 3
+"4" => 4
+"5" => 5
+"6" => 6
+"7" => 7
+"8" => 8
+"9" => 9
+"10" => 10
+"A" => 11
+"J" => 10
+"Q" => 10
+"K" => 10
+
+O jogo deverá embaralhar as cartas acima. O jogo deve pedir para o jogador virar uma cartão, quando ele virar, deverá apresentar o score de acordo com a carta.
+
+Obs.: O "A" terá o valor 1 quando tiver outro número já virado.
+
+Segue exemplo abaixo:
+["A"]                                           ==>  11
+["A", "J"]                                    ==>  21
+["A", "10", "A"]                         ==>  12
+["5", "3", "7"]                           ==>  15
+["5", "4", "3", "2", "A", "K"]   ==>  25'''
